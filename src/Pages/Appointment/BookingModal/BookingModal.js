cb3953a660e5b26fcc12e417d4d77a3a512ef307
Nasirul-Islam <nasirulislam866@gmail.com>
2021-11-08 20:16:12 +0600

    date fixed

--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -43,7 +43,7 @@ const BookingModal = ({ openBooking, handleBookingClose, booking, date, setBooki
             date: date.toLocaleDateString()
         }
         // send to the server
-        fetch('http://localhost:5000/appointments', {
+        fetch('https://morning-beach-24256.herokuapp.com/appointments', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
